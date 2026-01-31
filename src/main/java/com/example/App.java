6925890e51f83febe2b6c31262148bc362640569
ChathupaSandeep <chathupa.dewasirinarayana@revocaresolutions.com>
2026-01-31 18:06:42 +0530

    minor fixes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,9 @@ package main.java.com.example;
 
 public class App {
 
-    public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) throws Exception {
 
-        Calculator calc = new Calculator(); 
+        Calculator calc = new Calculator();
 
         System.out.println(calc.calculate(10, 5, "add-again")); 
         
